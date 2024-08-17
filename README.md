3700deb99196222a53993b35041d85c33683bea5
Atemu <git@atemu.net>
2024-08-17 23:11:47 +0200

    provide app output

--- README.md
+++ README.md
@@ -4,5 +4,7 @@ To be filled by anyone who fucks up using Nix.
 
 Simply run `nix build github:Atemu/nix-fuck-up-assessment-form` to receive your copy.
 
+Alternatively, you can run `nix run github:Atemu/nix-fuck-up-assessment-form` to open the fuck-up assessment form in your favourite PDF viewer.
+
 > [!NOTE]
 > This is a joke
--- flake.nix
+++ flake.nix
@@ -16,14 +16,24 @@
       system:
       let
         pkgs = nixpkgs.legacyPackages.${system};
+        inherit (nixpkgs) lib;
+        packages = self.packages.${system};
       in
       {
         packages = {
-          form = pkgs.callPackage ./package.nix {
-            inherit self;
-          };
+          form = pkgs.callPackage ./package.nix { inherit self; };
 
           default = self.packages.${system}.form;
+        };
+        apps = {
+          default = {
+            type = "app";
+            program = toString (
+              pkgs.writeShellScript "open-fuckup-assessment" ''
+                exec ${lib.getExe' pkgs.xdg-utils "xdg-open"} ${packages.default}
+              ''
+            );
+          };
         };
       }
     );
